335ac88571b87171c618cd644586d5a723145062
Juan Cruz Alvarez <juancruzab07@gmail.com>
2025-07-30 21:42:46 -0300

    agregando estilos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,9 @@ function NavBar() {
     <nav>
       <h1>Mi Tienda</h1>
       <ul>
-        <li>Inicio</li>
-        <li>Productos</li>
-        <li>Contacto</li>
+        <li><a href="">Inicio</a></li>
+        <li><a href="">Productos</a></li>
+        <li><a href="">Contacto</a></li>
       </ul>
       <CartWidget />
     </nav>
