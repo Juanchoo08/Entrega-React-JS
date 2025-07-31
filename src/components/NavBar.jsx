950a29061f50abcb121bfeb5d337c6dd5b8e485f
Juan Cruz Alvarez <juancruzab07@gmail.com>
2025-07-30 21:53:49 -0300

    agregando # a href

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,9 @@ function NavBar() {
     <nav>
       <h1>Mi Tienda</h1>
       <ul>
-        <li><a href="">Inicio</a></li>
-        <li><a href="">Productos</a></li>
-        <li><a href="">Contacto</a></li>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Contacto</a></li>
       </ul>
       <CartWidget />
     </nav>
